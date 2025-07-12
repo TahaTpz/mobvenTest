cb7c1981353903c2f2ce6fdb376e84f0de92a1d2
TahaTpz <tahatopuz19@gmail.com>
2025-07-12 18:08:35 +0300

    Initial commit: Xcode proje yapısı ve .gitignore eklendi

---
+++ mobvenTestTests/mobvenTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  mobvenTestTests.swift
+//  mobvenTestTests
+//
+//  Created by Taha Topuz on 12.07.2025.
+//
+
+import Testing
+@testable import mobvenTest
+
+struct mobvenTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
